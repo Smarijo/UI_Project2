55a431b254fef30b127db8a55768de5e304abace
Sara Marijolovic <smmarijo@gmail.com>
2023-11-10 17:53:03 -0500

    Added UI and Computer Graphics pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import Home from '/pages/Home'
 import Senior_Design from '/pages/Senior_Design'
+import UI from '/pages/UI'
+import Computer_Graphics from '/pages/Computer_Graphics'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/courses/senior-design" element={<Senior_Design />} />
+          <Route path="/courses/ui" element={<UI />} />
+          <Route path="/courses/computer-graphics" element={<Computer_Graphics />} />
           {/* <Route path="/shop" element={<Shop />} /> */}
         </Routes>
       </BrowserRouter>
