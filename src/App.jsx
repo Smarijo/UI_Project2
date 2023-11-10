d4faf6e83f818ff4a220b73dc407ddf0995a2680
Sara Marijolovic <smmarijo@gmail.com>
2023-11-10 12:53:42 -0500

    given course files and secondary pages for site

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
+import Home from '/pages/Home'
+// import Shop from './pages/Shop'
 
 function App() {
 
-  return
+  return(
+    <>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/shop" element={<Shop />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+    </>
+  )
 }
 
 export default App
